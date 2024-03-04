0171bf0e508e92f66852a9bfa5e77d47b939ec59
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-04 19:11:59 -0400

    Creè el Contexto en su carpeta DAL

---
+++ Robelin_Concepcion_P2_Ap1.Api/DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace Robelin_Concepcion_P2_Ap1.Api.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Accesorios> Accesorios { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Accesorios>().HasData(new List<Accesorios>()
+    {
+            new Accesorios() { AccesorioId = 1, Descripción = "Camara Trasera"},
+            new Accesorios() { AccesorioId = 2, Descripción = "Pantalla Interior"},
+            new Accesorios() { AccesorioId = 3, Descripción = "Interior en Piel" },
+            new Accesorios() { AccesorioId = 4, Descripción = "Sun Roof" },
+            new Accesorios() { AccesorioId = 5, Descripción = "Aros de Lujo"},
+    });
+        }
+    }
+}
