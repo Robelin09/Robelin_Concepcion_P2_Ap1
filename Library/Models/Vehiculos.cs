5cb10eed94d6d9e5cb3aed1388450b8e37296240
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-04 18:55:37 -0400

    Creé el modelo con su clase Vehiculos

---
+++ Library/Models/Vehiculos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models
+{
+    public class Vehiculos
+    {
+        [Key]
+        public int VehiculoId { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage ="Ingrese una descripción")]
+        public string? Descripción { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "El Costo debe ser mayor que cero.")]
+        public decimal Costo { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "El Gasto debe ser mayor que cero.")]
+        public decimal Gastos { get; set; }
+
+        [ForeignKey("VehiculoId")]
+        public ICollection<VehiculosDetalle> vehiculosDetalle { get; set; } = new List<VehiculosDetalle>();
+
+    }
+}
