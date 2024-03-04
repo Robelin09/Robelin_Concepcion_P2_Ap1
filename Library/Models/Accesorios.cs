04765e9fd1d3883547c7eb83fdeb063235d122d7
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-04 18:56:06 -0400

    Creé la clase accesorios en el Modelo

---
+++ Library/Models/Accesorios.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Accesorios
+    {
+        [Key]
+        public int AccesorioId { get; set; }
+
+        [Required(ErrorMessage ="Descripción Requerida")]
+
+        public string Descripción { get; set; }
+    }
+}
